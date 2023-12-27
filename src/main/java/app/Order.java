5953279028e4b9a665a5c4b5201b243c91ac8395
Rc0824 <chenpinrays@gmail.com>
2023-12-27 21:51:55 +0800

    update order etc.

--- src/main/java/app/Order.java
+++ src/main/java/app/Order.java
@@ -1,5 +1,52 @@
 package app;
 
+import java.sql.Timestamp;
+import org.json.JSONObject;
+import java.util.*;
+
 public class Order {
     
+    private int id;
+    private Timestamp order_time;
+    private int total;
+    private String captcha;
+    private ArrayList<Cart> orderlist;
+
+    public Order(int id, Timestamp order_time, int total, String captcha) {
+        this.id = id;
+        this.order_time = order_time;
+        this.total = total;
+        this.captcha = captcha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Timestamp getOrder_time() {
+        return order_time;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    public ArrayList<Cart> getOrderList() {
+        return orderlist;
+    }
+
+    public JSONObject getOrderInfo() {
+        JSONObject jso = new JSONObject();
+        jso.put("id", getId());
+        jso.put("order_time", getOrder_time());
+        jso.put("total", getTotal());
+        jso.put("captcha", getCaptcha());
+        return jso;
+    }
+
+
 }
---
+++ src/main/java/app/OrderHelper.java
@@ -0,0 +1,162 @@
+package app;
+
+import java.sql.*;
+import java.util.Collections;
+
+import org.json.*;
+import tools.*;
+import util.DBMgr;
+
+public class OrderHelper {
+
+    private static OrderHelper oh;
+    private Connection conn = null;
+    private PreparedStatement pres = null;
+
+    public static OrderHelper getHelper() {
+        if (oh == null) {
+            oh = new OrderHelper();
+        }
+        return oh;
+    }
+
+    public JSONObject getFood() {
+        Order o = null;
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `tbl_order` INNER JOIN `tbl_order_food_connect` ON `tbl_order`.`order_id` = `tbl_order_food_connect`.`order_id` INNER JOIN `tbl_food` ON `tbl_order_food_connect`.`food_id` = `tbl_food`.`food_id` WHERE `tbl_order`.`order_id` = ?";
+            
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 從結果集中讀取食物名稱 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+                int id = rs.getInt("order_id");
+                int food_id = rs.getInt("food_id");
+
+                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
+                
+                jsa.put(food_id);
+            }
+            
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    
+    public JSONObject getAll() {
+        Order o = null;
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `tbl_order`";
+            
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+                int id = rs.getInt("order_id");
+                Timestamp order_time = rs.getTimestamp("order_datetime");
+                int total = rs.getInt("order_total");
+                String captcha= rs.getString("order_capctha");
+                
+                /** 將每一筆商品資料產生一名新Product物件 */
+                o = new Order(id, order_time, total, captcha);
+                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
+                jsa.put(o.getOrderInfo());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+
+
+}
