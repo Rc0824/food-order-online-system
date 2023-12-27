47269f5efc6e4c81d6ed954dedcbc3d8114bc3e7
Rc0824 <chenpinrays@gmail.com>
2023-12-27 19:39:52 +0800

    update search food

--- src/main/java/app/Cart.java
+++ src/main/java/app/Cart.java
@@ -2,4 +2,26 @@ package app;
 
 public class Cart {
     
+    private int id;
+    private int total;
+    private int food_id;
+
+    public Cart(int id, int total, int food_id) {
+        this.id = id;
+        this.total = total;
+        this.food_id = food_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getFood_id() {
+        return food_id;
+    }
+
 }
